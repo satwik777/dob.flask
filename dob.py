1ee3d7d9db667696f12b287b3ceee64f83a223eb
unknown <satwiks173@gmail.com>
2023-09-28 21:52:22 +0530

    t# Please enter the commit message for your changes. Lines starting

--- dob.py
+++ dob.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, make_response
 from datetime import date
 import sqlite3
 
@@ -17,7 +17,6 @@ cursor.execute('''
 conn.commit()
 conn.close()
 
-# Default data (can be removed if you want to start with an empty database)
 default_data = {
     "John": date(2018, 12, 13),
     "Alice": date(2015, 2, 25),
@@ -26,7 +25,6 @@ default_data = {
     "vasanth": date(1999, 9, 30),
 }
 
-# Function to insert default data into the database
 def insert_default_data():
     conn = sqlite3.connect('birthday_data.db')
     cursor = conn.cursor()
@@ -41,11 +39,10 @@ def find_nearest_birthday():
     nearest_birthday = None
     nearest_name = None
 
-    # Open the database connection
+    
     conn = sqlite3.connect('birthday_data.db')
     cursor = conn.cursor()
 
-    # Query the 'birthdays' table
     cursor.execute("SELECT name, birthdate FROM birthdays WHERE birthdate >= ? ORDER BY birthdate ASC LIMIT 1", (today,))
     row = cursor.fetchone()
 
@@ -53,50 +50,75 @@ def find_nearest_birthday():
         nearest_name, nearest_birthday_str = row
         nearest_birthday = date.fromisoformat(nearest_birthday_str)  # Convert 'YYYY-MM-DD' string to date
 
-    # Close the database connection
     conn.close()
 
     return nearest_name, nearest_birthday
 
-@app.route('/viewdata')
+@app.route('/viewdata',methods=['GET'])
 def view_data():
-    # Fetch all birthday data from the database
     conn = sqlite3.connect('birthday_data.db')
     cursor = conn.cursor()
     cursor.execute("SELECT id, name, birthdate FROM birthdays")
     data = cursor.fetchall()
     conn.close()
-    print(data)
-    return render_template('all_birthday.html', data=data)
+    # Check for a specific header that is commonly sent by Postman
+    user_agent = request.headers.get('User-Agent')
+    if 'Postman' in user_agent:
+        return jsonify(data)
+    else:
+        # Request is from a web browser, return HTML response
+        return render_template('all_birthday.html', data=data)
 
 @app.route('/')
 def index():
     nearest_name, nearest_birthday = find_nearest_birthday()
     return render_template('index.html', nearest_name=nearest_name, nearest_birthday=nearest_birthday)
 
-@app.route('/no_upcoming_birthday')
-def no_upcoming_birthday():
-    return render_template('upcoming_birthday.html')
+#@app.route('/no_upcoming_birthday')
+#def no_upcoming_birthday():
+#
+#    return render_template('upcoming_birthday.html')
 
-# ... (other route handlers)
 @app.route('/add_birthday', methods=['POST'])
 def add_birthday():
-    name = request.form['name']
-    birthdate = request.form['birthdate']
+    if request.content_type == 'application/json':
+        try:
+            data = request.json  # Parse JSON data from the request
+            name = data['name']
+            birthdate = data['birthdate']
 
-    # Insert the new birthday into the database
-    conn = sqlite3.connect('birthday_data.db')
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO birthdays (name, birthdate) VALUES (?, ?)", (name, birthdate))
-    conn.commit()
-    conn.close()
+            conn = sqlite3.connect('birthday_data.db')
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO birthdays (name, birthdate) VALUES (?, ?)", (name, birthdate))
+            conn.commit()
+            conn.close()
 
-    # Redirect to the page displaying all birthday data
-    return redirect('/viewdata')
-# Edit an existing birthday entry
+            response = make_response("Success", 200)
+            return response
+        except Exception as e:
+            # Handle any errors that may occur, e.g., invalid JSON or missing keys
+            return jsonify({'error': str(e)}), 400
+    elif request.content_type == 'application/x-www-form-urlencoded':
+        try:
+            name = request.form['name']
+            birthdate = request.form['birthdate']
+
+            conn = sqlite3.connect('birthday_data.db')
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO birthdays (name, birthdate) VALUES (?, ?)", (name, birthdate))
+            conn.commit()
+            conn.close()
+
+            return redirect('/viewdata')
+        except Exception as e:
+            # Handle any errors that may occur with form data
+            return jsonify({'error': str(e)}), 400
+    else:
+        return jsonify({'error': 'Unsupported Content-Type'}), 400
+
+
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_birthday(id):
-    # Fetch the existing data for editing
     conn = sqlite3.connect('birthday_data.db')
     cursor = conn.cursor()
     print(id)
@@ -105,32 +127,44 @@ def edit_birthday(id):
     conn.close()
     print(data)
     if data is None:
-        # Handle the case where the ID does not exist
-        # You can redirect to an error page or show an error message
         return "Birthday not found"
 
     if request.method == 'POST':
         name = request.form['name']
         birthdate = request.form['birthdate']
 
-        # Update the entry in the database
         conn = sqlite3.connect('birthday_data.db')
         cursor = conn.cursor()
         cursor.execute("UPDATE birthdays SET name=?, birthdate=? WHERE id=?", (name, birthdate, id))
         conn.commit()
         conn.close()
 
-        # Redirect back to viewdata page after editing
         return redirect('/viewdata')
 
-    # If data is not None, proceed with rendering the edit form
     return render_template('edit_birthday.html', id=id, name=data[0], birthdate=data[1])
 
+# Additional functionality to edit birthday using JSON
+@app.route('/edit_json/<int:id>', methods=['POST'])
+def edit_birthday_json(id):
+    try:
+        data = request.json  # Parse JSON data from the request
+        name = data['name']
+        birthdate = data['birthdate']
+
+        conn = sqlite3.connect('birthday_data.db')
+        cursor = conn.cursor()
+        cursor.execute("UPDATE birthdays SET name=?, birthdate=? WHERE id=?", (name, birthdate, id))
+        conn.commit()
+        conn.close()
+
+        return jsonify({'message': 'Birthday updated successfully'})
+    except Exception as e:
+        # Handle any errors that may occur when parsing JSON or updating data
+        return jsonify({'error': str(e)}), 400
 
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete_birthday(id):
     print("Hi")
-    # Delete the entry from the database
     conn = sqlite3.connect('birthday_data.db')
     cursor = conn.cursor()
     cursor.execute("DELETE FROM birthdays WHERE id=?", (id,))
@@ -142,3 +176,5 @@ def delete_birthday(id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
